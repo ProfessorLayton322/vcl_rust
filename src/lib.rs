757899a5bf3b989ef1a54b5ce13278ac4627d238
fedorbykov <fibykov@edu.hse.ru>
2024-05-13 05:36:26 +0300

    Functions refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ mod tests {
 
     #[test]
     fn test_basic() {
-        assert_eq!(Vec4f::size(), 4);
+        assert_eq!(Vec4f::LEN, 4);
 
         let a = Vec4f::from_scalar(32.0);
         let b = Vec4f::new(1.0, 2.0, 3.0, 4.0);
@@ -19,7 +19,7 @@ mod tests {
 
         let arr : [f32; 4] = [-2.0, 1.0, 3.0, -4.0];
         let mut d = Vec4f::default();
-        d.load(arr.as_ptr());
+        d.load(&arr);
         assert_eq!(d, [-2.0, 1.0, 3.0, -4.0]);
     }
 
@@ -64,12 +64,12 @@ mod tests {
         assert_eq!(sqrt(Vec4f::new(0.0, 1.0, 2.0, 3.0)),
             [0.0, 1.0, f32::sqrt(2.0), f32::sqrt(3.0)]);
 
-        assert_eq!(square(Vec4f::new(-1.0, 1.0, 2.0, 3.0)),
+        assert_eq!(Vec4f::new(-1.0, 1.0, 2.0, 3.0).squared(),
             [1.0, 1.0, 4.0, 9.0]);
 
-        assert_eq!(pow(Vec4f::new(-1.0, 2.0, 3.0, 1.5), 2),
+        assert_eq!(Vec4f::new(-1.0, 2.0, 3.0, 1.5).pow(2),
             [1.0, 4.0, 9.0, 1.5 * 1.5]);
-        assert_eq!(pow(Vec4f::new(-1.0, 2.0, 3.0, 1.5), -1),
+        assert_eq!(Vec4f::new(-1.0, 2.0, 3.0, 1.5).pow(-1),
             [-1.0, 0.5, 1.0 / 3.0, 1.0 / 1.5]);
 
         assert_eq!(round(Vec4f::new(1.0, 1.4, 1.5, 1.6)),
@@ -78,7 +78,7 @@ mod tests {
 
     fn compare_approx_vec4f(vec: &Vec4f, expected : [f32; 4]) {
         let mut arr = [0.0f32; 4];
-        vec.store(arr.as_mut_ptr());
+        vec.store(&mut arr);
         for i in 0..4 {
             assert!(f32::abs(arr[i] - expected[i]) < 0.001f32);
         }
@@ -126,7 +126,7 @@ mod tests {
     }
 
     #[test]
-    fn test_insert_extract() {
+    fn test_insert_get() {
         let mut a = Vec4f::default();
         a.insert(0, 6.0);
         a.insert(1, 7.0);
@@ -136,62 +136,70 @@ mod tests {
         a.insert(2, 5.0);
         assert_eq!(a, [6.0, 7.0, 5.0, 23.0]);
 
-        assert_eq!(a.extract(0), 6.0);
-        assert_eq!(a.extract(1), 7.0);
-        assert_eq!(a.extract(2), 5.0);
-        assert_eq!(a.extract(3), 23.0);
+        assert_eq!(a.get(0), 6.0);
+        assert_eq!(a.get(1), 7.0);
+        assert_eq!(a.get(2), 5.0);
+        assert_eq!(a.get(3), 23.0);
     }
 
-    use aligned_array::{Aligned, A16};
+    fn select_aligned(buffer: &mut [f32; 5]) -> &mut [f32] {
+        if (buffer.as_ptr() as usize) % 16 == 0 {
+            return &mut buffer[..4];
+        }
+        return &mut buffer[1..];
+    }
+
     #[test]
     fn test_load_store() {
         let arr : [f32; 4] = [-2.0, 1.0, 4.0, 5.0];
         let mut a = Vec4f::default();
-        a.load(arr.as_ptr());
+        a.load(&arr);
         assert_eq!(a, [-2.0, 1.0, 4.0, 5.0]);
 
-        let aligned : Aligned<A16, _> = Aligned([-1.0, 2.0, 3.0, -2.0]);
-        assert!(aligned.as_ptr() as usize % 16 == 0);
+        let some_array : [f32; 4] = [-1.0, 2.0, 3.0, -2.0];
+
+        let mut unaligned = [0.0f32; 5];
+        let mut aligned = select_aligned(&mut unaligned);
+        aligned.copy_from_slice(&some_array);
+
         let mut b = Vec4f::default();
-        b.load_a(aligned.as_ptr());
-        assert_eq!(b, [-1.0, 2.0, 3.0, -2.0]);
+        b.load_aligned(aligned);
+        assert_eq!(b, some_array);
 
-        let mut aligned_buffer : Aligned<A16, _> = Aligned([0.0f32; 4]);
-        assert!(aligned_buffer.as_ptr() as usize % 16 == 0);
-        b.store_a(aligned_buffer.as_mut_ptr());
-        assert_eq!(aligned, aligned_buffer);
+        b.store_aligned(aligned);
+        assert_eq!(aligned, some_array);
 
-        let mut another_aligned : Aligned<A16, _> = Aligned([0.0f32; 4]);
-        assert!(another_aligned.as_ptr() as usize % 16 == 0);
-        b.store_nt(another_aligned.as_mut_ptr());
-        assert_eq!(aligned, another_aligned);
+        b.store_aligned_nocache(aligned);
+        assert_eq!(aligned, some_array);
 
         let mut buffer = [0.0f32; 4];
-        b.store_partial(1, buffer.as_mut_ptr());
+        b.store_partial(&mut buffer[..1]);
         assert_eq!(buffer, [-1.0, 0.0, 0.0, 0.0]);
-        b.store_partial(2, buffer.as_mut_ptr());
+        b.store_partial(&mut buffer[..2]);
         assert_eq!(buffer, [-1.0, 2.0, 0.0, 0.0]);
-        b.store_partial(3, buffer.as_mut_ptr());
+        b.store_partial(&mut buffer[..3]);
         assert_eq!(buffer, [-1.0, 2.0, 3.0, 0.0]);
-        b.store_partial(4, buffer.as_mut_ptr());
+        b.store_partial(&mut buffer);
         assert_eq!(buffer, [-1.0, 2.0, 3.0, -2.0]);
 
         let another_arr : [f32; 4] = [-10.0, 3.0, -2.0, 7.0];
         let mut c = Vec4f::default();
 
-        c.load_partial(0, another_arr.as_ptr());
+        c.load_partial(&another_arr[0..0]);
         assert_eq!(c, [0.0, 0.0, 0.0, 0.0]);
-        c.load_partial(1, another_arr.as_ptr());
+        c.load_partial(&another_arr[..1]);
         assert_eq!(c, [-10.0, 0.0, 0.0, 0.0]);
-        c.load_partial(2, another_arr.as_ptr());
+        c.load_partial(&another_arr[..2]);
         assert_eq!(c, [-10.0, 3.0, 0.0, 0.0]);
-        c.load_partial(3, another_arr.as_ptr());
+        c.load_partial(&another_arr[..3]);
         assert_eq!(c, [-10.0, 3.0, -2.0, 0.0]);
-        c.load_partial(4, another_arr.as_ptr());
+        c.load_partial(&another_arr);
         assert_eq!(c, [-10.0, 3.0, -2.0, 7.0]);
 
+        let too_large_array = [0.0f32; 5];
+
         let mut d = Vec4f::new(-3.0, 2.0, 1.0, 11.0);
-        d.load_partial(5, another_arr.as_ptr());
+        d.load_partial(&too_large_array);
         assert_eq!(d, [-3.0, 2.0, 1.0, 11.0]);
         d.cutoff(3);
         assert_eq!(d, [-3.0, 2.0, 1.0, 0.0]);

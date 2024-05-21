e40fc9fc09fa57d2c0f9c85fa06160bf0c71a9db
fedorbykov <fibykov@edu.hse.ru>
2024-05-21 13:02:15 +0300

    Fix benchmarks

--- benches/vcl_benchmark.rs
+++ benches/vcl_benchmark.rs
@@ -1,58 +1,54 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use vcl_rust::Vec4f;
 
-fn add(n: u32) {
+fn add(n: u32) -> Vec4f {
     let a = Vec4f::new(2.0, -1.0, 3.0, 4.0);
     let mut b = Vec4f::default();
     for _ in 0..n {
         b += a;
     }
+    b
 }
 
-fn div(n: u32) {
+fn div(n: u32) -> Vec4f {
     let a = Vec4f::new(2.0, -1.0, 3.0, 4.0);
     let mut b = Vec4f::from_scalar(1.0);
     for _ in 0..n {
         b /= a;
     }
+    b
 }
 
-fn mul(n: u32) {
+fn mul(n: u32) -> Vec4f {
     let a = Vec4f::new(2.0, -1.0, 3.0, 4.0);
     let mut b = Vec4f::from_scalar(1.0);
     for _ in 0..n {
         b *= a;
     }
+    b
 }
 
-fn pow(n: u32) {
-    let a = Vec4f::new(2.0, -1.0, 3.0, 4.0);
-    for _ in 0..n {
-        a.pow(100);
-    }
+fn pow(data: &mut [Vec4f]) {
+    data.iter_mut().for_each(|vec| *vec = vec.pow(100));
 }
 
-fn sqrt(n: u32) {
-    let a = Vec4f::new(31.0, 15.0, 111.0, 87.0);
-    for _ in 0..n {
-        a.sqrt();
-    }
+fn sqrt(data: &mut [Vec4f]) {
+    data.iter_mut().for_each(|vec| *vec = vec.sqrt());
 }
 
-fn horizontal(n: u32) {
-    let a = Vec4f::new(31.0, 15.0, 111.0, 87.0);
-    for _ in 0..n {
-        a.horizontal_add();
-    }
+fn horizontal(data: &[Vec4f], ) -> f32 {
+    data.iter().map(|vec| vec.horizontal_add()).sum()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("add 10", |b| b.iter(|| add(black_box(10))));
     c.bench_function("div 10", |b| b.iter(|| div(black_box(10))));
     c.bench_function("mul 10", |b| b.iter(|| mul(black_box(10))));
-    c.bench_function("pow 10", |b| b.iter(|| pow(black_box(10))));
-    c.bench_function("sqrt 10", |b| b.iter(|| sqrt(black_box(10))));
-    c.bench_function("horizontal 10", |b| b.iter(|| horizontal(black_box(10))));
+
+    let data = vec![Vec4f::new(1.0, 2.0, 3.0, 4.0); 10];
+    c.bench_function("pow 10", |b| b.iter(|| pow(black_box(&mut data.clone()))));
+    c.bench_function("sqrt 10", |b| b.iter(|| sqrt(black_box(&mut data.clone()))));
+    c.bench_function("horizontal 10", |b| b.iter(|| horizontal(black_box(&data.clone()))));
 }
 
 criterion_group!(benches, criterion_benchmark);
